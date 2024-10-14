74fcd0e1bb9513d124cc7feda4db5b50519f7b25
HP <HP@DESKTOP-2M4VFDM>
2024-10-14 10:43:20 +0530

    method

---
+++ mavenproject2/src/main/java/method/Static_Method.java
@@ -0,0 +1,19 @@
+package method;
+
+public class Static_Method {
+	
+	static String name;
+	static int age;
+	
+	public static void display()
+	{
+	String name;
+	int age;
+	}
+
+	public static void display1() 
+	{
+	String name = "Adam";
+	int age = 13;
+	}
+}
