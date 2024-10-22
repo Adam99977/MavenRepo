839fff2f8c83c93bc541615c26a21cea9812b017
HP <HP@DESKTOP-2M4VFDM>
2024-10-22 07:20:27 +0530

    Excel_Read

---
+++ mavenproject1/src/main/java/excelread/Excel_Read_Main.java
@@ -0,0 +1,13 @@
+package excelread;
+
+import java.io.IOException;
+
+public class Excel_Read_Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(Excel_ReadEx01.getstringdata(1, 0));
+		System.out.println(Excel_ReadEx01.getintegerdata(1, 1));
+
+	}
+
+}
